bcb9aae99a73b43b6d07a7a628c843a4e6a11a54
Raik Ilves <raik.ilves@gmail.com>
2016-07-26 13:55:21 +0300

    init

---
+++ Vagrantfile
@@ -0,0 +1,22 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure("2") do |config|
+  config.vm.box = "ubuntu/trusty64"
+
+  config.vm.network "private_network", ip: "10.1.2.3"
+  # the host path will be created if it does not exist.
+  config.vm.synced_folder "./www", "/var/www", create: true, owner: "www-data", group: "www-data"
+
+  # Read line from file and remove line breaks
+  hosts = File.readlines("./vhosts-list").map(&:chomp)
+  # Filter out comments starting with "#"
+  hosts.grep(/\A[^#]/)
+
+  # Pass the found host names to the hostsupdater plugin so it can perform magic.
+  config.hostsupdater.aliases = hosts
+
+  config.vm.provision :shell, :path => "provision.sh"
+
+
+end
---
+++ provision.sh
@@ -0,0 +1,85 @@
+#!/usr/bin/env bash
+
+# Unlock the root and give it a password? (YES/NO)
+ROOT=YES
+
+# Add vhosts
+sudo cp -Rv /vagrant/vhosts/* /etc/apache2/sites-available/
+
+avail=/etc/apache2/sites-available/$1.conf
+enabled=/etc/apache2/sites-enabled/
+site=`ls /vagrant/vhosts/`
+
+if [ "$#" != "1" ]; then
+    echo "Available virtual hosts: $site"
+    sudo a2ensite $site
+else
+
+    if test -e $avail; then
+        sudo ln -s $avail $enabled
+    else
+         "$avail virtual host does not exist! Please create one!$site"
+    fi
+fi
+
+echo "Booting the machine..."
+sudo service apache2 restart
+
+if [ ! -f /var/log/firsttime ];
+then
+    sudo touch /var/log/firsttime
+
+    # Set credentials for MySQL
+    sudo debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password password vagrant"
+    sudo debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password_again password vagrant"
+
+    # Install packages
+    sudo apt-get update
+    sudo apt-get -y install mysql-server-5.5 php5-mysql apache2 git libapache2-mod-php5 php5-curl
+
+    # Install WordPress specific recommendations
+    sudo apt-get -y install php5-cli php5-dev php5-fpm php5-cgi php5-mysql php5-xmlrpc php5-curl php5-gd php5-imagick php-apc php-pear php5-imap php5-mcrypt php5-pspell
+
+    # Create WordPress database
+    # sudo mysql -uroot -p$DB_PASSWORD -e "CREATE DATABASE $DB_NAME"
+
+    # Add timezones to database
+    mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -uroot -pvagrant mysql
+
+    # Download WordPress
+    # sudo git clone https://github.com/WordPress/WordPress.git -b $REPO /home/vagrant/shared/wordpress
+    
+    # Create wp-config.php
+    # sudo mv /home/vagrant/shared/wordpress/wp-config-sample.php /home/vagrant/shared/wordpress/wp-config.php
+    # sudo sed -i "s/database_name_here/$DB_NAME/" /home/vagrant/shared/wordpress/wp-config.php
+    # sudo sed -i "s/username_here/root/" /home/vagrant/shared/wordpress/wp-config.php
+    # sudo sed -i "s/password_here/$DB_PASSWORD/" /home/vagrant/shared/wordpress/wp-config.php    
+
+    # Allow URL rewrites
+    sudo a2enmod rewrite
+
+    # Setup Some PHP-FPM Options
+    sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
+    sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
+    sudo sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php5/apache2/php.ini
+    sudo sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php5/apache2/php.ini
+    sudo sed -i "s/post_max_size = .*/post_max_size = 100M/" /etc/php5/apache2/php.ini
+
+    # php5-mysql comes w/mysql drivers, but we still have to update php.ini to use them.
+    sudo sed -i 's/;pdo_odbc.db2_instance_name/;pdo_odbc.db2_instance_name\nextension=pdo_mysql.so/' /etc/php5/apache2/php.ini
+    
+    # Install curl & wp-cli
+    sudo apt-get -y install curl
+    cd ~/ && curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    sudo mv wp-cli.phar /usr/bin/wp && sudo chmod +x /usr/bin/wp
+
+    # Restart machine, obviously
+    sudo service apache2 restart
+fi
+
+# Unlock root and set password  
+if [ $ROOT = 'YES' ]
+then
+    sudo usermod -U root
+    echo -e "password\npassword" | sudo passwd root
+fi
